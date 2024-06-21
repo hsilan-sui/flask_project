ead3dab7c0c79b544e3c9df12d3eb0b66f75a0ba
hsilan-sui <suihsilan1101@icloud.com>
2024-06-21 14:24:33 +0800

    settle css js to static and index.html to templates, use render_template()method to render index.html in the browser

--- web_server/server.py
+++ web_server/server.py
@@ -1,7 +1,15 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__) 
 
-@app.route("/") 
-def hello_user(): 
-    return "<p>妳好 sui~~~~~~hhh</p>"
+# @app.route("/") 
+# def hello(): 
+#     return render_template('index.html')
+
+# @app.route('/<myname>')
+# def hello(myname=None):
+#   return render_template('index.html', name=myname)
+
+@app.route('/<myname>/<int:post_id>')
+def hello(myname=None, post_id=None):
+  return render_template('index.html', name=myname, post_id=post_id)
