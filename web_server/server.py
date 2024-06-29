5c604d1a632e579dba3b93566a4562be5ab4a26c
hsilan-sui <suihsilan1101@icloud.com>
2024-06-29 15:06:05 +0800

    dynamic the url parementer and take the repeatition structure from html content to be base.html layout

--- web_server/server.py
+++ web_server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__) 
 
@@ -6,14 +6,18 @@ app = Flask(__name__)
 def home(): 
     return render_template('index.html')
 
-@app.route("/about.html") 
-def about(): 
-    return render_template('about.html')
+@app.route('/<string:page_name>') 
+def html_page(page_name): 
+    return render_template(page_name)
 
-@app.route("/works.html") 
-def works(): 
-    return render_template('works.html')
+# @app.route('/about.html') 
+# def about(): 
+#     return render_template('/about.html')
 
-@app.route("/contact.html") 
-def contact(): 
-    return render_template('contact.html')
+# @app.route("/works.html") 
+# def works(): 
+#     return render_template('works.html')
+
+# @app.route("/contact.html") 
+# def contact(): 
+#     return render_template('contact.html')
